2283849daf03d5cd36e95a60811aab852a50e5b7
VLAD EMINOVICI <veminovici@hotmail.com>
2023-10-16 19:50:44 +0300

    Refactor a bit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,16 @@
-import { loggingMiddleware } from '@middlewares/middleware-logging';
+import { loggingMiddleware } from '@lib/middlewares/middleware-logging';
 import type { NextFetchEvent, NextRequest } from 'next/server'
 import * as MW from 'next-auth/middleware'
-import { authMiddleware } from '@middlewares/middleware-auth';
+import { authMiddleware } from '@lib/middlewares/middleware-auth';
 
-// // This function can be marked `async` if using `await` inside
+// This function can be marked `async` if using `await` inside
 export async function middleware(request: MW.NextRequestWithAuth, _next: NextFetchEvent) {
-
   const mw = authMiddleware();
-  //let res = await MW.default(request, _next);
   let res = mw(request, _next);
   return res;
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/about/:path*'],
 }
-
-// export { default } from "next-auth/middleware"
-// export const config = { matcher: ["/about/:path*", "/profile/:path*", "/dashboard/:path*"] }
